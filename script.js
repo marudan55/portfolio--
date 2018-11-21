6f156ea6cc2c6d1cd09595db2f9aef3075dea92f
marudan55 <45030239+marudan55@users.noreply.github.com>
2018-11-21 18:58:00 +0900

    スクロールトップボタン、メニューボタン追加

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ $(function(){
     return false;
   });
 });
+
+$(function(){
+  $('#hbg-menu li a').click(function(){
+    $('#hbg-menu').slideUp('fast');
+    $('#menu-btn').removeClass('active');
+  });
+});
+
+//ページトップボタン
+$(function(){
+  $(window).scroll(function(){
+    var now = $(this).scrollTop();
+    if(now > 50) {
+      $('#page-top').slideDown('slow');
+    } else {
+      $('#page-top').slideUp('slow');
+    }
+  });
+  $('#page-top').click(function(){
+    $('html,body').animate({scrollTop: 0} ,'slow');
+  });
+});
